5e9ace919dfbb2bc9813f19b0a327b963e8c2d8f
trannhatlan <trannhatlan@hotmail.com>
2016-08-04 22:41:43 -0600

    add BE nad FE validations

--- Gighub/ViewModels/GigFormViewModel.cs
+++ Gighub/ViewModels/GigFormViewModel.cs
@@ -1,17 +1,29 @@
 ﻿using Gighub.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Gighub.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+
+        [Required]
+        [ValiaTime]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; }
+
         public IEnumerable<Genre> Genres { get; set; }
-        public DateTime DateTime => DateTime.Parse($"{Date} {Time}"); // using expression body
+
+        public DateTime GetDateTime() => DateTime.Parse($"{Date} {Time}"); // using expression body
     }
 
 }
