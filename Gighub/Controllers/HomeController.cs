9545436ea42140accb570b6eef9bbb11830cbac1
trannhatlan <trannhatlan@hotmail.com>
2016-08-06 18:07:30 -0600

    add style for gig date, fix dropdow style

--- Gighub/Controllers/HomeController.cs
+++ Gighub/Controllers/HomeController.cs
@@ -20,6 +20,7 @@ namespace Gighub.Controllers
             // eager loading???
             var upcomingGigs = _context.Gigs
                 .Include(g => g.Artist)
+                .Include(g => g.Genre)
                 .Where(g => g.DateTime > DateTime.Now);
               
             return View(upcomingGigs);
