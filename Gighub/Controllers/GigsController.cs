440b2a7d726fda0e6110c5a17651312788225bf8
trannhatlan <trannhatlan@hotmail.com>
2016-08-13 12:18:54 -0600

    add my upcoming gigs

--- Gighub/Controllers/GigsController.cs
+++ Gighub/Controllers/GigsController.cs
@@ -1,6 +1,7 @@
 ﻿using Gighub.Models;
 using Gighub.ViewModels;
 using Microsoft.AspNet.Identity;
+using System;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
@@ -14,6 +15,18 @@ namespace Gighub.Controllers
         public GigsController()
         {
             _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = _context.Gigs
+                .Where(g => g.ArtistId == userId && g.DateTime > DateTime.Now)
+                .Include(g => g.Genre)
+                .ToList();
+
+            return View(gigs);
         }
 
         [Authorize]
@@ -68,7 +81,7 @@ namespace Gighub.Controllers
             _context.Gigs.Add(gig);
             _context.SaveChanges();
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Mine", "Gigs");
         }
     }
 }
